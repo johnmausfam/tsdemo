8a5d1a904f2d6fef720f89a92f0c15659ce9f795
fam <fam1923@gmail.com>
2018-07-15 18:57:23 +0800

    module

--- src/jscomponents/FuncButton.d.ts
+++ src/jscomponents/FuncButton.d.ts
@@ -1,26 +1,27 @@
 import * as React from 'react';
-
-declare interface FuncButtonProps {
-    className?:string,
-	enable?:boolean,
-	disableAfterClick?:boolean,
-	text:string,
-	value?:any,
-	onClick?:(e:{value:string})=>void,
-	iconName?:string,
-	buttonStyle?:string
+declare module './FuncButton.jsx'
+{
+	export interface FuncButtonProps {
+		className?:string,
+		enable?:boolean,
+		disableAfterClick?:boolean,
+		text:string,
+		value?:any,
+		onClick?:(e:{value:string})=>void,
+		iconName?:string,
+		buttonStyle?:string
+	}
+	
+	export default class FuncButton extends React.Component<FuncButtonProps, any> {
+	}
+	
+	export type IFuncButtonIcon = {
+		[key:string]:string
+	}
+	
+	export type IFuncButtonStyle = {
+		[key in "submit" | "cancel"]:string    
+	}
+	export const FuncButtonIcon:IFuncButtonIcon
+	export const FuncButtonStyle:IFuncButtonStyle
 }
-
-declare class FuncButton extends React.Component<FuncButtonProps, any> {
-}
-
-declare type IFuncButtonIcon = {
-    [key:string]:string
-}
-
-declare type IFuncButtonStyle = {
-    [key in "submit" | "cancel"]:string    
-}
-export default FuncButton
-export declare const FuncButtonIcon:IFuncButtonIcon
-export declare const FuncButtonStyle:IFuncButtonStyle
