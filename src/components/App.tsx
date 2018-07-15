67a9d1cb9fd406b08fd58ba26d8706f0777d102a
fam <fam1923@gmail.com>
2018-07-15 18:46:43 +0800

    A

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-import FuncButton,{FuncButtonIcon,FuncButtonStyle} from '../jscomponents/FuncButton'
+import FuncButton2,{FuncButtonIcon,FuncButtonStyle} from '../jscomponents/FuncButton'
 
 const App:React.StatelessComponent = ()=>(
     <div className="app">
         <h1>Hello!</h1>
-        <FuncButton className="mtbtn" text="myText" value="1" iconName={FuncButtonIcon.Ok} buttonStyle={FuncButtonStyle.submit} />
+        <FuncButton2 onClick={(e)=>alert(e.value)} className="mtbtn" text="myText" value="1" iconName={FuncButtonIcon.Ok} buttonStyle={FuncButtonStyle.submit} />
     </div>
 )
 
